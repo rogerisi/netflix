71127121b42f1a39258e20c8422aabd788dcaf97
rogerioLozano <rogerio.luis.lozano@hotmail.com>
2020-06-25 14:48:30 -0300

    refactor: update main.html & add question.js

--- main/script/main.js
+++ main/script/main.js
@@ -1,42 +1,41 @@
-const conteudosPerguntasFrequentes = [`A Netflix é um serviço de transmissão online que oferece uma ampla variedade de séries, filmes e documentários premiados em milhares de aparelhos conectados à internet.
-<br><br>
-Você pode assistir a quantos filmes e séries quiser, quando e onde quiser, sem comerciais – tudo por um preço mensal bem acessível. Aqui você sempre encontra novidades. A cada semana, adicionamos novas séries e filmes.`,
-`Assista à Netflix no seu celular, tablet, Smart TV, notebook ou aparelho de streaming por uma taxa mensal única. Os planos variam de R$21,90 a R$45,90 por mês. Sem contrato nem taxas extras.`,
+const sectionMainIndex= ['h1','h3','h5'] 
 
-`Assista onde quiser, o quanto quiser e em um número ilimitado de aparelhos. Faça login com sua conta Netflix em netflix.com para começar a assistir no computador ou em qualquer aparelho conectado à Internet compatível com o aplicativo Netflix, como Smart TVs, smartphones, tablets, aparelhos de streaming e videogames.
+const sectionMain = {
+    h1:"Filmes séries, e muito mais.Sem limites.",  
+    h3:"Assista quando quiser. Cancele quando quiser.", 
+    h5:"Pronto para assistir? Informe seu email para criar ou reiniciar sua assinatura"
+} 
 
-Você também pode baixar a sua série favorita com o aplicativo Netflix para iOS, Android ou Windows 10. Use downloads para levar a Netflix para onde quiser sem precisar de conexão com a Internet. Leve a Netflix com você para qualquer lugar.`,
+sectionMainIndex.forEach(e=> {
+    let idSectionMain = `section__main__${e}` 
+    document.getElementById(idSectionMain).innerHTML = sectionMain[e] 
+}) 
 
-`A Netflix é flexível. Não há contratos nem compromissos. Você pode cancelar a sua conta na internet com apenas dois cliques. Não há taxa de cancelamento – você pode começar ou encerrar a sua assinatura a qualquer momento.`,
-
-`A Netflix tem um grande catálogo de filmes, documentários, séries, originais Netflix premiados e muito mais. Assista o quanto quiser, quando quiser.`];
-
-const mostrarTextoPorPosicao = (arg) => {
-    
-    var classe ="content"+arg
-    var teste = document.getElementById(classe); 
+const mostrarTextoPorPosicao = (arg) => {  
+    let classe  =  `content${arg}`
+    let elemento =  document.getElementById(classe); 
     let texto = conteudosPerguntasFrequentes[arg]; 
-    var styleDisplay = "";  
-    styleDisplay = teste.style.display
+    let styleDisplay = ``;  
+    styleDisplay = elemento.style.display
     
-    if(!styleDisplay){
-        styleDisplay = "none"; 
+    if (!styleDisplay) {
+        styleDisplay = `none`; 
     }
 
-    if(styleDisplay == "none"){
-        teste.style.display = "block";    
-        var idCoteudo ="conteudo" + arg 
+    if (styleDisplay == `none`) {
+        elemento.style.display = `block`;    
+        let idCoteudo =`conteudo${arg}` 
         document.getElementById(idCoteudo).innerHTML = texto;
     } else {
-      teste.style.display = "none";   
+        elemento.style.display = `none`;   
     }
 }
 
-const collapse = (param) =>{
+const collapse = (param) => {
     theTarget = event.target; 
     const value = param;     
     if(value){
-        theTarget.addEventListener("click",mostrarTextoPorPosicao(value)) 
+        theTarget.addEventListener(`click`,mostrarTextoPorPosicao(value)) 
     }  
 }
 
